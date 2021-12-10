4e619f14086585be8a18fcb849fbc9e0213a90b1
Yujie Xia <xiayjchn@gmail.com>
2021-12-11 00:59:37 +0800

    sleep in each loop

--- src/operator.rs
+++ src/operator.rs
@@ -1,3 +1,5 @@
+use tokio::time::{sleep, Duration};
+
 use crate::storage::Storage;
 
 pub struct Operator {
@@ -12,10 +14,12 @@ impl Operator {
     pub async fn run(&self) {
         loop {
             let _state = self.storage.snapshot();
-            // For each instance.
+            // For each instance (stage != Deleting)
             // 1. Ensure namespace is created.
             // 2. Ensure rootfs is initialized.
-            // 3. Ensure pod is running.
+            // 3. Ensure service is created.
+            // 4. Ensure pod is running.
+            sleep(Duration::from_secs(1)).await;
         }
     }
 }
