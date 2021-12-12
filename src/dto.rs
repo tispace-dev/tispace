24a33b8323b0fac31b71d88fc57f03de915d9ca8
hi-rustin <rustin.liu@gmail.com>
2021-12-12 12:28:04 +0800

    use pub
    Signed-off-by: hi-rustin <rustin.liu@gmail.com>

--- src/dto.rs
+++ src/dto.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Default, Serialize, Deserialize)]
 #[serde(default)]
-crate struct UserLoginRequest {
+pub struct UserLoginRequest {
     crate username: String,
     crate password: String,
 }
