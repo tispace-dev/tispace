c5061fc3cdbbba9589566b41c2386102eeecdf0d
Yujie Xia <xiayjchn@gmail.com>
2021-12-15 17:39:47 +0800

    rename and add comments

--- scripts/init-rootfs.sh
+++ scripts/init-rootfs.sh
@@ -4,8 +4,12 @@ set -eux
 
 if [ ! -f /tmp/rootfs/usr ]; then
   set +e
+  # tar may throw a error like "tar: file changed as we read it".
+  # This is most likely due to the new output package in tmp directory.
+  # We ignore this error explicitly since we have excluded tmp directory.
   tar -cpzf /tmp/rootfs.tgz --warning=no-file-changed --exclude=./tmp --exclude=./init-rootfs.sh --one-file-system -C / .
   exitcode=$?
+  # exitcode 1 means "Some files differ", ignore it.
   if [ "$exitcode" != "0" ] && [ "$exitcode" != "1" ]; then
     exit "$exitcode"
   fi
