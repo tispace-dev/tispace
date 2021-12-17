cff5ffbe8335f1a77892c54496cf739e1315643a
二手掉包工程师 <rustin.liu@gmail.com>
2021-12-17 23:23:19 +0800

    app: finish index page layout (#34)

---
+++ app/components/header.tsx
@@ -0,0 +1,27 @@
+import { Layout } from 'antd'
+import React from 'react'
+
+import styles from '../styles/Header.module.less'
+
+const { Header: AntDesignHeader } = Layout
+
+type Props = {
+  children: React.ReactNode
+}
+
+function Header({ children }: Props) {
+  return (
+    <AntDesignHeader>
+      <div className={styles.logo}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/logo.svg" alt="Logo" />
+      </div>
+      <title>TiSpace</title>
+      <meta name="description" content="TiSpace" />
+      <link rel="icon" href="/favicon.ico" />
+      {children}
+    </AntDesignHeader>
+  )
+}
+
+export default Header
