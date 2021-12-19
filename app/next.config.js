6f56265f2ed7604fe04842bf1c4bf74b0efac1bc
hi-rustin <rustin.liu@gmail.com>
2021-12-19 13:09:24 +0800

    app: fix gh pages broken issue

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,12 @@
+const debug = process.env.NODE_ENV !== 'production'
 const withAntdLess = require('next-plugin-antd-less')
 
-module.exports = withAntdLess({
-  webpack(config) {
-    return config
-  },
-})
+module.exports = {
+  ...withAntdLess({
+    webpack(config) {
+      return config
+    },
+  }),
+  assetPrefix: !debug ? '/tispace/' : '',
+  basePath: !debug ? '/tispace/' : '',
+}
